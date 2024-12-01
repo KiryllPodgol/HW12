390d7aa7c6d115f76fa34a5dbbcb8e24361e7388
Kirill <podgol04@gmail.com>
2024-12-01 21:10:53 +0300

    Изменение подхода

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,6 +4,7 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target; 
     public Vector3 offset; 
+    public float smoothSpeed = 0.125f; // Скорость сглаживания
 
     void Start()
     {
@@ -17,8 +18,9 @@ public class CameraFollow : MonoBehaviour
     {
         if (target != null)
         {
-            
-            transform.position = target.position + offset;
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
         }
     }
 }
